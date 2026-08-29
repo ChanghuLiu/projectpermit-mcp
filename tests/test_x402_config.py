fb36a71d60bc6fbbb4cc366cd1e2aa92eed2e7ae
liuchanghu2018 <122949289+ChanghuLiu@users.noreply.github.com>
2026-08-29 07:43:47 -0400

    Test x402 batch price configuration

--- tests/test_x402_config.py
+++ tests/test_x402_config.py
@@ -23,7 +23,36 @@ class X402ConfigTest(unittest.TestCase):
         with patch.dict(os.environ, env, clear=True):
             s = load_x402_settings()
             self.assertTrue(s['enabled'])
+            self.assertEqual('$0.10', s['batch_price'])
             validate_x402_settings(s)
+
+    def test_batch_price_can_be_overridden_independently(self):
+        env = {
+            'PROJECTPERMIT_X402_ENABLED': 'true',
+            'PROJECTPERMIT_X402_PRICE_USD': '$0.10',
+            'PROJECTPERMIT_X402_BATCH_PRICE_USD': '$0.50',
+            'PROJECTPERMIT_X402_NETWORK': 'eip155:84532',
+            'PROJECTPERMIT_X402_PAY_TO': '0x0000000000000000000000000000000000000001',
+            'PROJECTPERMIT_X402_FACILITATOR_URL': 'https://x402.org/facilitator',
+        }
+        with patch.dict(os.environ, env, clear=True):
+            s = load_x402_settings()
+            self.assertEqual('$0.10', s['price'])
+            self.assertEqual('$0.50', s['batch_price'])
+            validate_x402_settings(s)
+
+    def test_enabled_rejects_invalid_batch_price(self):
+        env = {
+            'PROJECTPERMIT_X402_ENABLED': 'true',
+            'PROJECTPERMIT_X402_PRICE_USD': '$0.10',
+            'PROJECTPERMIT_X402_BATCH_PRICE_USD': '0.50',
+            'PROJECTPERMIT_X402_NETWORK': 'eip155:84532',
+            'PROJECTPERMIT_X402_PAY_TO': '0x0000000000000000000000000000000000000001',
+            'PROJECTPERMIT_X402_FACILITATOR_URL': 'https://x402.org/facilitator',
+        }
+        with patch.dict(os.environ, env, clear=True):
+            with self.assertRaisesRegex(RuntimeError, 'BATCH_PRICE'):
+                validate_x402_settings(load_x402_settings())
 
     def test_enabled_rejects_legacy_network_name(self):
         env = {
